27d1f18f786713fe194b1e86999a1d42463c2636
alvis <alomemu@gmail.com>
2024-05-04 19:41:01 +0100

    productLayout without css

---
+++ templates/productLayout.html.php
@@ -0,0 +1,37 @@
+<main>
+<div class = "productContainer">
+
+<div class="mainImage">
+    <img src="placeholder-1.png" alt="Placeholder Image" />
+</div>
+
+
+<div class="priceDesc">
+<div class ="price">
+    <p>$0000.00</p>
+</div>
+<div class="productDesc">
+    <h3>Description</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, necessitatibus aut. Possimus eius veritatis laboriosam, fuga iusto quas molestiae architecto accusantium suscipit, sapiente dolores. Quisquam cumque eius atque voluptate a.</p>
+</div>
+<div class="moreinfo">
+<h4>More info</h4>
+</div>
+</div>
+
+
+<div class="imagesReviews">
+<div class="extraImages">
+<img src="placeholder-1.png" alt="Placeholder Image" />
+<img src="placeholder-2.png" alt="Placeholder Image" />
+<img src="placeholder-3.png" alt="Placeholder Image" />
+</div>
+<div class="stars">
+</div>
+<div class="reviews">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere distinctio obcaecati ex id eaque nihil commodi quaerat accusantium laborum? Aliquid ad eum doloribus ut laboriosam tempora aut natus facere provident.</p>
+</div>
+</div>
+
+</div>
+</main>
