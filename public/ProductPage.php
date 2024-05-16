998775af04931ae8d349b7139057279515f3fab3
alvis <alomemu@gmail.com>
2024-05-16 20:01:40 +0100

    sear

--- public/ProductPage.php
+++ public/ProductPage.php
@@ -10,15 +10,6 @@
     $Title = basename(__FILE__, '.php');
     //this changes the name of the tab to this current file's name
 
-    $pdo = setMysqlDatabase('csy2088','csy2088','csy2088');
-    if (isset($_GET["searchFilterCategory"])){
-        $productQuery = 'SELECT * FROM products WHERE Category = :Category';
-        $productQueryData = ['Category' => $_GET["searchFilterCategory"] ];
-        $productData = queryMysqlDatabase($pdo, $productQuery, $productQueryData)->fetchAll(PDO::FETCH_ASSOC);
-    }else{
-        $productQuery = 'SELECT * FROM csy2088.products';
-        $productData = queryMysqlDatabase($pdo, $productQuery, null)->fetchAll(PDO::FETCH_ASSOC);
-    }
    
 
     require "../templates/nonMain/Head.html.php";
